0003dc776e6b5f24cdea71fbbaadd8413225ec4a
owwo <owwo@outlook.com>
2016-12-08 13:15:46 +0800

    添加 bootstrap

--- src/app.js
+++ src/app.js
@@ -1,3 +1,5 @@
+import 'bootstrap/dist/css/bootstrap.css';
+
 let foo = () => { console.log('hello'); };
 
 foo();
--- webpack.config.js
+++ webpack.config.js
@@ -10,11 +10,28 @@ module.exports = {
   module: {
     loaders: [
       { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' },
+      { test: /\.css$/, loader: 'style-loader!css-loader' },
+      {
+        test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+        loader: 'url',
+        query: {
+          limit: 10000,
+          name: path.resolve(__dirname, 'dist/', '/img/[name].[hash:7].[ext]'),
+        },
+      },
+      {
+        test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
+        loader: 'url',
+        query: {
+          limit: 10000,
+          name: path.resolve(__dirname, 'dist/', '/fonts/[name].[hash:7].[ext]'),
+        },
+      },
     ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      filename: path.resolve(__dirname, 'dist/index.html'),
+      filename: path.resolve(__dirname, 'dist/', 'index.html'),
       template: path.resolve(__dirname, 'index.html'),
     }),
   ],
